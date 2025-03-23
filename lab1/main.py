e5855ae1937b89b14c942c6d0c86c65749cf065b
andziuba <andziuba@users.noreply.github.com>
2025-03-23 12:38:42 +0100

    Create main.py

---
+++ lab1/main.py
@@ -0,0 +1,16 @@
+from bbs import bbs
+from tests import single_bit_test, series_test, long_series_test, poker_test
+
+
+def main():
+    num_of_bits = 20000
+    bits = bbs(num_of_bits)
+
+    print("Single Bit Test:", "passed" if single_bit_test(bits) else "failed")
+    print("Series Test:", "passed" if series_test(bits)  else "failed")
+    print("Long Series Test:", "passed" if long_series_test(bits)  else "failed")
+    print("Poker Test:", "passed" if poker_test(bits)  else "failed")
+
+
+if __name__ == "__main__":
+    main()
